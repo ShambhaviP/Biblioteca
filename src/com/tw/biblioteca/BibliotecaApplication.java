a10985469ebdee5043fdf968644dbdb76d6faf4a
ShambhaviP <shambhap@thoughtworks.com>
2015-09-03 11:59:22 +0530

    Modified BibliotecaApplication class to print the welcome message when the application is run

--- src/com/tw/biblioteca/BibliotecaApplication.java
+++ src/com/tw/biblioteca/BibliotecaApplication.java
@@ -9,6 +9,6 @@ public class BibliotecaApplication {
 
     public static void main(String args[]) {
         BibliotecaApplication bibliotecaApplication = new BibliotecaApplication();
-        bibliotecaApplication.start();
+        System.out.println(bibliotecaApplication.start());
     }
 }
