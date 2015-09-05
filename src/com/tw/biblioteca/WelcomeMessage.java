2728055f466b30faf3b90502b90edd6640394bc4
ShambhaviP <shambhap@thoughtworks.com>
2015-09-05 10:18:08 +0530

    removed all classes,started with an entirely new approach, created a test class for welcome message,and a corresponding WelcomeMessage class with a method to display the message,added a spec for that method

---
+++ src/com/tw/biblioteca/WelcomeMessage.java
@@ -0,0 +1,16 @@
+//to generate a welcome message for Biblioteca Application
+package com.tw.biblioteca;
+
+public class WelcomeMessage {
+
+    private String message;
+
+    WelcomeMessage(String message) {
+        this.message = message;
+    }
+
+
+    public void showMessage() {
+        System.out.println(message);
+    }
+}
---
+++ test/com/tw/biblioteca/WelcomeMessageTest.java
@@ -0,0 +1,33 @@
+package com.tw.biblioteca;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class WelcomeMessageTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @Test
+    public void shouldReturnAValidWelcomeMessage() {
+        WelcomeMessage welcomeMessage = new WelcomeMessage("Welcome to Biblioteca!");
+
+        welcomeMessage.showMessage();
+
+        assertEquals("Welcome to Biblioteca!\n", outContent.toString());
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
+}
