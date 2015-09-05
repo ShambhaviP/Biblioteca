31a63330010dbeee5c2e28bebbf89653fef4a698
ShambhaviP <shambhap@thoughtworks.com>
2015-09-05 10:36:16 +0530

    Created a BibliotecaApplication class which has the main method to launch the application on console

---
+++ src/com/tw/biblioteca/BibliotecaApplication.java
@@ -0,0 +1,10 @@
+//Biblioteca application launcher
+package com.tw.biblioteca;
+
+public class BibliotecaApplication {
+
+    public static void main(String args[]) {
+        WelcomeMessage welcomeMessage = new WelcomeMessage("Hello,User!Welcome to Biblioteca!");
+        welcomeMessage.showMessage();
+    }
+}
