71b61f97643cf8f6762e41f6644454ea8cfd6059
ShambhaviP <shambhap@thoughtworks.com>
2015-09-06 10:09:39 +0530

    For user story 3, created a test class for Book,added spec to return book's title,author and year of publish, created a book class with the required method

---
+++ src/com/tw/biblioteca/Book.java
@@ -0,0 +1,18 @@
+package com.tw.biblioteca;
+
+public class Book {
+
+    private String bookName;
+    private String author;
+    private int yearOfPublish;
+
+    Book(String bookName, String author, int yearOfPublish) {
+        this.bookName = bookName;
+        this.author = author;
+        this.yearOfPublish = yearOfPublish;
+    }
+
+    public String bookDetails() {
+        return bookName+"    "+author+"    "+yearOfPublish;
+    }
+}
---
+++ test/com/tw/biblioteca/BookTest.java
@@ -0,0 +1,16 @@
+package com.tw.biblioteca;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BookTest {
+
+   @Test
+   public void shouldReturnBookTitleAndAuthorAndYearOfPublish() {
+       Book book = new Book("Twilight", "Stephanie Meyer", 2006);
+
+       assertEquals("Twilight    Stephanie Meyer    2006", book.bookDetails());
+
+   }
+}
