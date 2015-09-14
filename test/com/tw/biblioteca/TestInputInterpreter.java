5de382f341e419ad917d219eb0ff64f470fc055e
ShambhaviP <shambhap@thoughtworks.com>
2015-09-14 09:14:50 +0530

    made a correction in TestInputInterpreter.java for successful build

--- test/com/tw/biblioteca/TestInputInterpreter.java
+++ test/com/tw/biblioteca/TestInputInterpreter.java
@@ -5,7 +5,6 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.ExpectedSystemExit;
-
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
@@ -67,7 +66,7 @@ public class TestInputInterpreter {
         InputInterpreter inputInterpreter = new InputInterpreter(bookLibrary);
 
         exit.expectSystemExit();
-        inputInterpreter.interpret("3");
+        inputInterpreter.interpret("4");
     }
 
     @After
