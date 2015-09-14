da1508fcceefb177322e5bf820757c7f5251d22f
ShambhaviP <shambhap@thoughtworks.com>
2015-09-14 15:17:12 +0530

    added spec in TestCheckOutBookOption for unsuccessful checkout message

--- test/com/tw/biblioteca/TestCheckOutBookOption.java
+++ test/com/tw/biblioteca/TestCheckOutBookOption.java
@@ -46,6 +46,23 @@ public class TestCheckOutBookOption {
         assertEquals("Thank You! Enjoy the book.\n", outContent.toString());
     }
 
+    @Test
+    public void shouldPrintTheUnsuccessfulCheckOutMessageWhenABookIsNotCheckedOutSuccessfully() {
+        Book book1 = new Book("Harry Potter", "J K Rowling", 1999);
+        Book book2 = new Book("The KiteRunner", "Khaled Hosseini", 2005);
+        ArrayList<Book> availableBooks = new ArrayList<>();
+        availableBooks.add(book1);
+        availableBooks.add(book2);
+        ArrayList<Book> checkedOutBooks = new ArrayList<>();
+        BookLibrary bookLibrary = new BookLibrary(availableBooks, checkedOutBooks);
+        CheckOutBookOption checkOutBookOption = new CheckOutBookOption(bookLibrary);
+        ByteArrayInputStream inputStream = new ByteArrayInputStream("Anna Karenina".getBytes());
+        System.setIn(inputStream);
+        checkOutBookOption.performOperation();
+
+        assertEquals("That book is not available.\n", outContent.toString());
+    }
+
     @After
     public void cleanUpStreams() {
         System.setOut(null);
