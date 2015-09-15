0971f4a088212d7b0f92b3ca0ffae7b5343a5870
ShambhaviP <shambhap@thoughtworks.com>
2015-09-15 12:04:03 +0530

    wrote a spec to display appropriate message on unsuccessful checkout of a movie in TestCheckOutMovieOption

--- test/com/tw/biblioteca/TestCheckOutMovieOption.java
+++ test/com/tw/biblioteca/TestCheckOutMovieOption.java
@@ -37,6 +37,23 @@ public class TestCheckOutMovieOption {
         assertEquals("Thank You! Enjoy the movie.\n", outContent.toString());
     }
 
+    @Test
+    public void shouldDisplayAppropriateMessageOnUnsuccessfulCheckOutOfAMovie() {
+        Movie movie1 = new Movie("Titanic", 1997, "James Cameron", "8.5");
+        Movie movie2 = new Movie("Roman Holiday", 1953, "William Wyler", "9.0");
+        ArrayList<Movie> movies = new ArrayList<>();
+        movies.add(movie1);
+        movies.add(movie2);
+        MovieLibrary movieLibrary = new MovieLibrary(movies);
+
+        CheckOutMovieOption checkOutMovieOption = new CheckOutMovieOption(movieLibrary);
+        ByteArrayInputStream inputStream = new ByteArrayInputStream("Fast And Furious".getBytes());
+        System.setIn(inputStream);
+        checkOutMovieOption.performOperation();
+
+        assertEquals("That movie is not available.\n", outContent.toString());
+    }
+
     @After
     public void cleanUpStreams() {
         System.setOut(null);
