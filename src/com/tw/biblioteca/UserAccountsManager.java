801632dfffa83ad3659acefdc15771d3963de65f
ShambhaviP <shambhap@thoughtworks.com>
2015-09-16 06:42:28 +0530

    created a test class for UserAccountsManager wherein wrote a spec for having a predefined set of users, created UserAccountsManager class with the required constructor and getter

---
+++ src/com/tw/biblioteca/UserAccountsManager.java
@@ -0,0 +1,21 @@
+package com.tw.biblioteca;
+
+import java.util.ArrayList;
+
+public class UserAccountsManager {
+
+    private ArrayList<User> userAccounts = new ArrayList<>();
+
+    public UserAccountsManager() {
+        User user1 = new User("111-1111", "user1password");
+        User user2 = new User("222-2222", "user2password");
+        User user3 = new User("333-3333", "user3password");
+        userAccounts.add(user1);
+        userAccounts.add(user2);
+        userAccounts.add(user3);
+    }
+
+    public ArrayList<User> getUserAccounts() {
+        return userAccounts;
+    }
+}
---
+++ test/com/tw/biblioteca/TestUserAccountsManager.java
@@ -0,0 +1,24 @@
+package com.tw.biblioteca;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestUserAccountsManager {
+
+    @Test
+    public void shouldHaveAListOfPredefinedUsers() {
+        UserAccountsManager userAccountsManager = new UserAccountsManager();
+        User user1 = new User("111-1111", "user1password");
+        User user2 = new User("222-2222", "user2password");
+        User user3 = new User("333-3333", "user3password");
+        ArrayList<User> users = new ArrayList<>();
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
+
+        assertEquals(users, userAccountsManager.getUserAccounts());
+    }
+}
